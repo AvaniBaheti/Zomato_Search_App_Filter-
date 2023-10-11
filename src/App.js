c04a711946dd583a4228ed21257b6fe4f6960bba
avani_baheti <avanibaheti7@gmail.com>
2023-10-11 15:18:57 +0530

    changing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Search from './compo/Search';
+import Cards from './compo/Cards';
 function App() {
   return (
     <div className="App">
--- src/compo/Search.js
+++ src/compo/Search.js
@@ -1,22 +1,41 @@
-import React, { useState } from 'react'
-import './syyle.css'
-
-import FoodData from './FoodData'
-
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
+import React, { useEffect, useState } from 'react'
+import Fooddata from './FoodData'
+import "./syyle.css"
+import Form from 'react-bootstrap/Form'
+import Cards from './Cards'
 
 const Search = () => {
-    const [ans,seAns]= useState(FoodData)  
+
+    const [fdata, setFdata] = useState(Fooddata);
+    // console.log(fdata);
+
+    const [copydata, setCopyData] = useState([]);
+
+    // console.log(copydata);
+
+
+    
+
+
     const zomatologo = "https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
 
-  return (
-    <>
-     <div className="container d-flex justify-content-between align-items-center">
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            setCopyData(Fooddata);
+        }, 3000);
+
+    }, [])
+
+    return (
+        <>
+            <div className="container d-flex justify-content-between align-items-center">
                 <img src={zomatologo} style={{ width: "8rem", position: "relative", left: "2%", cursor: "pointer" }} alt="" />
                 <h2 style={{ color: "#1b1464", cursor: "pointer" }} className="mt-3">Search Filter App</h2>
             </div>
+
+
             <Form className='d-flex justify-content-center align-items-center mt-3'>
                 <Form.Group className=" mx-2 col-lg-4" controlId="formBasicEmail">
 
@@ -27,8 +46,17 @@ const Search = () => {
                 <button className='btn text-light col-lg-1' style={{ background: "#ed4c67" }}>Submit</button>
             </Form>
 
-    </>
-  )
+
+            <section className='iteam_section mt-4 container'>
+                <h2 className='px-4' style={{ fontWeight: 400 }}>Restaurants Open In Indore .</h2>
+
+                <div className="row mt-2 d-flex justify-content-around align-items-center">
+<Cards data={fdata}/>
+                </div>
+            </section>
+            
+        </>
+    )
 }
 
 export default Search
